353de5e43afdf44ccc5b755b467b15bd4dea48f9
Jan Borchmann <Jan.Borchmann@capitalone.com>
2017-11-04 17:24:40 -0400

    added more queries and ordered by page

--- sql_statements.sql
+++ sql_statements.sql
@@ -1,3 +1,5 @@
+----- FIRST PAGE -----
+
 -- donations
 SELECT sum(donation_amount) FROM donations WHERE donor_type='Individual'
 
@@ -6,6 +8,9 @@ SELECT sum(donation_amount) FROM donations WHERE donor_type='Organization'
 
 --program attendance
 SELECT count(*) FROM events WHERE program_ind = 1
+
+--event attendance
+SELECT count(*) from events where program_ind = 0
 
 --donor count
 SELECT member_count + non_member_count
@@ -18,8 +23,8 @@ from donations where donor_type = 'Individual') a
 SELECT sum(case when member_id = 999999999 and donor_type = 'Organization' then 1 else 0 end) as funder_count
 from donations
 
---event attendance
-SELECT count(*) from events where program_ind = 0
+
+-------- THIRD PAGE --------
 
 --top 5 visited events
 SELECT event_name,event_dt, attendee_count FROM (
@@ -28,6 +33,10 @@ WHERE program_ind = 0 GROUP BY 1,2,3
 ) a
 ORDER BY attendee_count DESC LIMIT 5
 
+-------- FOURTH PAGE ---------
+
+---top
+
 --attendance by program
 SELECT event_name, COUNT(*) as attendee_count FROM events
 WHERE program_ind = 1 GROUP BY 1 ORDER BY attendee_count desc
@@ -35,6 +44,8 @@ WHERE program_ind = 1 GROUP BY 1 ORDER BY attendee_count desc
 --funding by program
 SELECT program_funded, SUM(donation_amount) as donations FROM donations WHERE program_ind=1
 GROUP BY 1 ORDER BY donations desc
+
+----bottom
 
 --attendance by month and program
 SELECT event_name,SUBSTRING(event_dt,1,7) as month, COUNT(*) as attendance FROM events
@@ -46,11 +57,21 @@ SELECT program_funded,SUBSTRING(donation_date,1,7) as month, SUM(donation_amount
 WHERE program_ind=1
 GROUP BY 1,2 ORDER BY 1 asc,2 asc
 
+
+------- FITFTH PAGE BOTTOM --------
+
 -- donations over time
 SELECT SUBSTRING(donation_date,1,7) as month, SUM(donation_amount) FROM donations
 WHERE program_ind=1
 GROUP BY 1 ORDER BY 1 asc
 
+
+--------- SIXTH PAGE --------
+
 -- membership over time
-SELECT SUBSTRING(join_dt,1,7) as month, COUNT(*) FROM members
+SELECT SUBSTRING(join_date,1,7) as month, COUNT(*) FROM members
 GROUP BY 1 ORDER BY 1
+
+--age of members
+SELECT (CURRENT_DATE - birth_date)/365::int, COUNT(*) FROM members
+GROUP BY 1 ORDER BY 1 ASC
