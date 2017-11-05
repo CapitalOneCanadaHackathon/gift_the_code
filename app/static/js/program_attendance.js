7919693609aa3f44f861f55e9fe4d89f1620b639
Kwok, Bjorn <Bjorn.Kwok@capitalone.com>
2017-11-05 09:52:34 -0500

    attendance bar chart

---
+++ app/static/js/program_attendance.js
@@ -0,0 +1,28 @@
+var chart = dc.barChart('#program_attendance');
+
+d3.json('program_data.json', function(error, data) {
+    if(error)
+        throw new Error(error);
+    var attendance_data   = data.data.attendance_by_program
+        ndx            = crossfilter(attendance_data),
+        programDimension = ndx.dimension(function(d) {return d.event_name;}),
+        sumGroup       = programDimension.group().reduceSum(function(d) {return d.attendee_count;});
+
+    chart
+        .width(768)
+        .height(380)
+        .margins({top: 30, right: 50, bottom: 40, left: 60})
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .ordinalColors(['#595097'])
+        .brushOn(false)
+        .xAxisLabel('Program')
+        .yAxisLabel('Attendance')
+        .dimension(programDimension)
+        .gap(10)
+        .barPadding(0.5)
+        .outerPadding(0.05)
+        .group(sumGroup);
+
+    chart.render();
+});
