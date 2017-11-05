d4237cf0504ef0a4ddd23116b97d25fe71f3bb89
Kwok, Bjorn <Bjorn.Kwok@capitalone.com>
2017-11-04 22:06:23 -0400

    init donation time series

---
+++ app/static/js/donation.js
@@ -0,0 +1,39 @@
+function refresh_charts() {
+    $.getJSON("/donation_api", function(data) {
+        var chart = dc.compositeChart("#donation_ts");
+
+        var data = data;
+        var dateFormat = d3.time.format("%Y-%m");
+        var ts_data = data.data.donations_over_time;
+        var ndx = crossfilter(ts_data);
+
+        ts_data.forEach(function (e) {
+            e.dd = dateFormat.parse(e.month);
+            e.year = d3.time.year(e.dd);
+            e.month = d3.time.month(e.dd); // pre-calculate month for better performance
+        });
+
+        var dateDim = ndx.dimension(function (d) {return d.dd;});
+        var donation_sum = dateDim.group().reduceSum(function(d) {return d.sum;});
+
+        var minDate = dateDim.bottom(1)[0].dd;
+        var maxDate = dateDim.top(1)[0].dd;
+
+        chart
+          .width(768)
+          .height(480)
+          .dimension(dateDim)
+          .x(d3.time.scale().domain([minDate,maxDate]))
+          .brushOn(false)
+          .elasticY(true)
+          .renderHorizontalGridLines(true)
+          .renderVerticalGridLines(true)
+          .margins({top: 30, right: 50, bottom: 40, left: 60})
+          .group(donation_sum, "Donations \(\$\)")
+          .valueAccessor(function (d) {return d.value;}).interpolate('basis-open')
+          .yAxisLabel("Donations \(\$\)")
+          .xAxisLabel("Date")
+          .legend(dc.legend().x(90).y(30).itemHeight(13).gap(5))
+
+        dc.renderAll();
+      });
